4192c86b1b8fb8920734640ee0a1345bbacd91e5
Felipe Levi <felipegpt19@gmail.com>
2025-10-17 08:23:42 -0300

    Moving pieces

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -8,7 +8,7 @@ public class ChessPosition {
 	private int row;
 
 	public ChessPosition(char column, int row) {
-		if(column < 'a' || column > 'h' || row < 1 || row > 8) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
 			throw new ChessException("Error instantiang ChessPosition. Valid values are from a1 to h8.");
 		}
 		this.column = column;
@@ -24,17 +24,16 @@ public class ChessPosition {
 	}
 
 	protected Position toPosition() {
-		return new Position(8 - row, column - 'a');	
+		return new Position(8 - row, column - 'a');
 	}
-	
+
 	protected static ChessPosition fromPosition(Position position) {
-		return new ChessPosition((char)('a' - position.getColumn()), 8 - position.getRow());
+		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
 	}
 
 	@Override
 	public String toString() {
 		return "" + column + row;
 	}
-	
-	
+
 }
